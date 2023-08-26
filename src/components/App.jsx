e3da6ca83bea881b051f651b150d3a09d85c07cb
Толочко Катерина <bantiktokinpakistan@gmail.com>
2023-08-26 09:04:57 +0300

    Fixed issues

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,12 +4,13 @@ import Filter from './Filter/Filter';
 import css from './App.module.css';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { selectContacts } from 'redux/selectors';
+import { selectError, selectIsLoading } from 'redux/selectors';
 import { fetchContacts } from 'redux/operations';
 
 export const App = () => {
   const dispatch = useDispatch();
-  const { isLoading, error } = useSelector(selectContacts);
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
 
   useEffect(() => {
     dispatch(fetchContacts());
